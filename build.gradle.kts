d7a2f1250c8a6e9e8b594f2a6a28644bc19fe1e8
Alexey Filimonov <filimonov.alks@gmail.com>
2024-02-23 02:30:05 +0300

    Add data base scheme

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,36 +6,57 @@ plugins {
     kotlin("jvm") version "1.9.22"
     kotlin("plugin.spring") version "1.9.22"
     kotlin("plugin.jpa") version "1.9.22"
+    kotlin("plugin.allopen") version "1.8.0"
 }
 
 group = "ru.filimonov"
 version = "0.0.1-SNAPSHOT"
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_21
-}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    val flywayVersion = "10.8.1"
+
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+//    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("org.modelmapper:modelmapper:3.1.1")
+    implementation("org.flywaydb:flyway-core:$flywayVersion")
+    runtimeOnly("org.flywaydb:flyway-database-postgresql:$flywayVersion")
     runtimeOnly("org.postgresql:postgresql")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude("junit", "junit")
+    }
+    testRuntimeOnly("org.flywaydb:flyway-database-postgresql:$flywayVersion")
+    testRuntimeOnly("org.postgresql:postgresql")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = "21"
+        jvmTarget = "17"
     }
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.Embeddable")
+    annotation("jakarta.persistence.MappedSuperclass")
 }
---
+++ src/main/resources/db/migration/V1_0__base_schema.sql
@@ -0,0 +1,128 @@
+CREATE TABLE "users"
+(
+    "uuid"         uuid PRIMARY KEY,
+    "created_date" timestamp NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "plants"
+(
+    "uuid"              uuid PRIMARY KEY,
+    "name"              text      NOT NULL,
+    "air_temp_min"      real,
+    "air_temp_max"      real,
+    "air_humidity_min"  real,
+    "air_humidity_max"  real,
+    "soil_moisture_min" real,
+    "soil_moisture_max" real,
+    "light_lux_min"     int,
+    "light_lux_max"     int,
+    "created_date"      timestamp NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "users_plants"
+(
+    "user_id" uuid NOT NULL,
+    "plant"   uuid NOT NULL,
+    PRIMARY KEY ("user_id", "plant")
+);
+
+CREATE TABLE "devices"
+(
+    "uuid"         uuid PRIMARY KEY,
+    "mac"          varchar(17) NOT NULL,
+    "plant"        uuid,
+    "user_id"      uuid        NOT NULL,
+    "created_date" timestamp   NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "air_humidity_readings"
+(
+    "uuid"      uuid PRIMARY KEY,
+    "reading"   real      NOT NULL,
+    "timestamp" timestamp NOT NULL DEFAULT (now()),
+    "device"    uuid      NOT NULL
+);
+
+CREATE TABLE "air_temp_readings"
+(
+    "uuid"      uuid PRIMARY KEY,
+    "reading"   real      NOT NULL,
+    "timestamp" timestamp NOT NULL DEFAULT (now()),
+    "device"    uuid      NOT NULL
+);
+
+CREATE TABLE "battery_charge_readings"
+(
+    "uuid"      uuid PRIMARY KEY,
+    "reading"   real      NOT NULL,
+    "timestamp" timestamp NOT NULL DEFAULT (now()),
+    "device"    uuid      NOT NULL
+);
+
+CREATE TABLE "light_lux_readings"
+(
+    "uuid"      uuid PRIMARY KEY,
+    "reading"   int       NOT NULL,
+    "timestamp" timestamp NOT NULL DEFAULT (now()),
+    "device"    uuid      NOT NULL
+);
+
+CREATE TABLE "soil_moisture_readings"
+(
+    "uuid"      uuid PRIMARY KEY,
+    "reading"   real      NOT NULL,
+    "timestamp" timestamp NOT NULL DEFAULT (now()),
+    "device"    uuid      NOT NULL
+);
+
+CREATE TABLE "water_reserve_readings"
+(
+    "uuid"         uuid PRIMARY KEY,
+    "enough_water" bool      NOT NULL,
+    "timestamp"    timestamp NOT NULL DEFAULT (now()),
+    "device"       uuid      NOT NULL
+);
+
+CREATE UNIQUE INDEX ON "devices" ("plant");
+
+CREATE INDEX ON "air_humidity_readings" ("timestamp");
+
+CREATE INDEX ON "air_temp_readings" ("timestamp");
+
+CREATE INDEX ON "battery_charge_readings" ("timestamp");
+
+CREATE INDEX ON "light_lux_readings" ("timestamp");
+
+CREATE INDEX ON "soil_moisture_readings" ("timestamp");
+
+CREATE INDEX ON "water_reserve_readings" ("timestamp");
+
+ALTER TABLE "users_plants"
+    ADD FOREIGN KEY ("user_id") REFERENCES "users" ("uuid") ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE "users_plants"
+    ADD FOREIGN KEY ("plant") REFERENCES "plants" ("uuid") ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE "devices"
+    ADD FOREIGN KEY ("plant") REFERENCES "plants" ("uuid") ON DELETE SET NULL ON UPDATE CASCADE;
+
+ALTER TABLE "devices"
+    ADD FOREIGN KEY ("user_id") REFERENCES "users" ("uuid") ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE "air_humidity_readings"
+    ADD FOREIGN KEY ("device") REFERENCES "devices" ("uuid") ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE "air_temp_readings"
+    ADD FOREIGN KEY ("device") REFERENCES "devices" ("uuid") ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE "battery_charge_readings"
+    ADD FOREIGN KEY ("device") REFERENCES "devices" ("uuid") ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE "light_lux_readings"
+    ADD FOREIGN KEY ("device") REFERENCES "devices" ("uuid") ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE "soil_moisture_readings"
+    ADD FOREIGN KEY ("device") REFERENCES "devices" ("uuid") ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE "water_reserve_readings"
+    ADD FOREIGN KEY ("device") REFERENCES "devices" ("uuid") ON DELETE CASCADE ON UPDATE CASCADE;
