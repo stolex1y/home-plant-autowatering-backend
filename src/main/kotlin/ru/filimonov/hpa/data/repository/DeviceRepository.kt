b2078ed6d6fc9aa730678de8c6d27be723139790
Alexey Filimonov <filimonov.alks@gmail.com>
2024-02-26 20:51:52 +0300

    Add findByMac method

--- src/main/kotlin/ru/filimonov/hpa/data/repository/DeviceRepository.kt
+++ src/main/kotlin/ru/filimonov/hpa/data/repository/DeviceRepository.kt
@@ -4,4 +4,6 @@ import org.springframework.data.repository.CrudRepository
 import ru.filimonov.hpa.data.model.DeviceEntity
 import java.util.*
 
-interface DeviceRepository : CrudRepository<DeviceEntity, UUID>
+interface DeviceRepository : CrudRepository<DeviceEntity, UUID> {
+    fun findByMac(mac: String): DeviceEntity?
+}
