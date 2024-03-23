e2270575819fe3845ffb9f5c10ae4c91a9df271a
Alexey Filimonov <filimonov.alks@gmail.com>
2024-03-24 00:48:31 +0300

    Add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.4"
-    kotlin("jvm") version "1.9.22"
+    kotlin("jvm") version "1.9.23"
     kotlin("plugin.spring") version "1.9.22"
     kotlin("plugin.jpa") version "1.9.22"
     kotlin("plugin.allopen") version "1.8.0"
@@ -19,24 +19,43 @@ repositories {
 dependencies {
     val flywayVersion = "10.8.1"
 
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.23")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.23")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.8.0")
+
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-//    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+    implementation("org.springframework.integration:spring-integration-mqtt:6.2.2")
+
+    implementation("org.springframework.security:spring-security-oauth2-resource-server")
+    implementation("org.springframework.security:spring-security-web")
+    implementation("org.springframework.security:spring-security-oauth2-jose")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-    implementation("org.modelmapper:modelmapper:3.1.1")
+
+    implementation("org.modelmapper:modelmapper:3.2.0")
+
     implementation("org.flywaydb:flyway-core:$flywayVersion")
-    implementation("org.springframework.integration:spring-integration-mqtt:6.2.2")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
     runtimeOnly("org.flywaydb:flyway-database-postgresql:$flywayVersion")
+
+    implementation("com.google.firebase:firebase-admin:9.2.0")
+
     runtimeOnly("org.postgresql:postgresql")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude("junit", "junit")
     }
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.3.0")
 }
 
 java {
