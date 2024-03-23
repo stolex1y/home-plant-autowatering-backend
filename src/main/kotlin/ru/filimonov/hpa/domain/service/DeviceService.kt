ffdca4cccfb4d698c19b374bf36f482d6f8547d0
Alexey Filimonov <filimonov.alks@gmail.com>
2024-03-24 01:47:23 +0300

    Add functions with user ownerships checks in device service

--- src/main/kotlin/ru/filimonov/hpa/domain/service/DeviceService.kt
+++ src/main/kotlin/ru/filimonov/hpa/domain/service/DeviceService.kt
@@ -1,7 +1,10 @@
 package ru.filimonov.hpa.domain.service
 
 import ru.filimonov.hpa.domain.model.Device
+import java.util.*
 
 interface DeviceService {
     fun getDeviceByMac(mac: String): Device?
+    fun getAllDevicesByUserId(userId: String): List<Device>
+    fun isUserDevice(userId: String, deviceId: UUID): Boolean
 }
