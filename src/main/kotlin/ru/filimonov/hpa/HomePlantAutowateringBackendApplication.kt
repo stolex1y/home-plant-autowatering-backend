ac7dae6011474faac96087102a0e650df4d0e678
Alexey Filimonov <filimonov.alks@gmail.com>
2024-03-24 01:44:59 +0300

    Add location setting by default

--- src/main/kotlin/ru/filimonov/hpa/HomePlantAutowateringBackendApplication.kt
+++ src/main/kotlin/ru/filimonov/hpa/HomePlantAutowateringBackendApplication.kt
@@ -1,10 +1,34 @@
 package ru.filimonov.hpa
 
+import jakarta.validation.constraints.NotBlank
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.context.properties.bind.ConstructorBinding
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import java.util.*
+
+private const val LOCATION_PROPS = "app.location"
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class HomePlantAutowateringBackendApplication {
+
+    @ConfigurationProperties(prefix = LOCATION_PROPS)
+    class LocationProperties @ConstructorBinding constructor(
+        @NotBlank
+        val timeZone: String,
+        @NotBlank
+        val locale: String
+    )
+
+    @Bean
+    fun setLocation(locationProperties: LocationProperties?) = CommandLineRunner {
+        TimeZone.setDefault(TimeZone.getTimeZone(locationProperties?.timeZone ?: "UTC"))
+        Locale.setDefault(Locale.forLanguageTag(locationProperties?.locale ?: "en-US"))
+    }
 }
 
 fun main(args: Array<String>) {
